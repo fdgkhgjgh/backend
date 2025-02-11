d8eca98259c4f2e10352f75259625d0e5f67c74b
fdgkhgjgh <whye@tutamail.com>
2025-02-12 01:07:48 +0800

    fdsf

--- models/Post.js
+++ models/Post.js
@@ -1,11 +1,5 @@
+// backend/models/Post.js
 const mongoose = require('mongoose');
-
-const commentSchema = new mongoose.Schema({
-    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    text: { type: String, required: true },
-    imageUrl: { type: String },
-    replies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }], // **ADD THIS LINE**
-}, { timestamps: true });
 
 const postSchema = new mongoose.Schema({
     title: { type: String, required: true },
@@ -17,7 +11,7 @@ const postSchema = new mongoose.Schema({
     downvotes: { type: Number, default: 0 },
     upvotedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', index: true }],
     downvotedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', index: true }],
-    comments: [commentSchema], // <--- ADD THIS LINE! This is the key fix!
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }], // Change to reference Comment model
 }, { timestamps: true });
 
 const Post = mongoose.model('Post', postSchema);
