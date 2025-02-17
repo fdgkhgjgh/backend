97b71716902f3ece43fc905882c4761a1fdc2817
fdgkhgjgh <whye@tutamail.com>
2025-02-18 01:32:47 +0800

    noti1

--- routes/posts.js
+++ routes/posts.js
@@ -10,19 +10,6 @@ const mongoose = require('mongoose'); // Ensure Mongoose is required
 const { postLimiter, commentLimiter } = require('../middleware/rateLimit'); // Import rate limiters
 const authenticateToken = require('../middleware/auth');
 
-// Middleware to verify JWT and protect routes
-const authenticateToken = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN
-
-    if (!token) return res.sendStatus(401); // Unauthorized
-
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403); // Forbidden
-        req.user = user;  //  Attach the user object to the request
-        next();
-    });
-};
 
 // Get all posts (with pagination)
 router.get('/', async (req, res) => {
